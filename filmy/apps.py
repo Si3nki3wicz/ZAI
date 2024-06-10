d29ac6fac26958cc854214549563f1458e1b9e79
Si3nki3wicz <59998044+Si3nki3wicz@users.noreply.github.com>
2024-06-11 01:07:09 +0200

    commit 9

--- filmy/apps.py
+++ filmy/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class FilmyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filmy'
+
+    def ready(self):
+        # signals are imported, so that they are defined and can be used
+        import filmy.signals

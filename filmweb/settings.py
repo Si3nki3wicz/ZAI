1591b12343e03dce1d3826d7de1ff755ec1495df
Si3nki3wicz <59998044+Si3nki3wicz@users.noreply.github.com>
2024-06-11 01:47:54 +0200

    commit 13

--- filmweb/settings.py
+++ filmweb/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -140,4 +141,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 1
+}
+
+GRAPHENE = {
+    "SCHEMA": "filmy.schema.schema"
 }
